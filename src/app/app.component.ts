e789c3c64f06aaadb96644e0f1345911b799b489
Rafael Bamert <rafael.bamert@gmail.com>
2016-10-03 21:25:00 +0200

    added angularfire2 service with connection test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {AngularFire, FirebaseListObservable} from 'angularfire2';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+    title = 'Firebase connection-test';
+
+    users: FirebaseListObservable<any[]>;
+
+    constructor(af: AngularFire) {
+        this.users = af.database.list('users');
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,18 @@ import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
 import {MaterialModule} from '@angular/material';
+import {AngularFireModule} from 'angularfire2';
 
 import {AppComponent} from './app.component';
+
+// Must export the config. @todo: move this to configService
+export const firebaseConfig = {
+    apiKey: "AIzaSyBtv1qf-Vq1jBtUzO0kVULiTkqab-ysurY",
+    authDomain: "diseasediary.firebaseapp.com",
+    databaseURL: "https://diseasediary.firebaseio.com",
+    storageBucket: "diseasediary.appspot.com",
+    messagingSenderId: "81460151035"
+};
 
 @NgModule({
     declarations: [
@@ -14,7 +24,8 @@ import {AppComponent} from './app.component';
         BrowserModule,
         FormsModule,
         HttpModule,
-        MaterialModule.forRoot()
+        MaterialModule.forRoot(),
+        AngularFireModule.initializeApp(firebaseConfig)
     ],
     providers: [],
     bootstrap: [AppComponent]
