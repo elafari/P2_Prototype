ba80585b660ec01196f26b962b68294e4d6f3a9c
Rafael Bamert <rafael.bamert@gmail.com>
2016-10-14 10:57:15 +0200

    playing around with patient cards

--- src/app/+patients/patients.component.ts
+++ src/app/+patients/patients.component.ts
@@ -3,7 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
     templateUrl: './patients.component.html',
     styles: [`
-        img {max-width: 100%}
+        img {
+            max-width: 100%;
+            width: 100%;
+            height: auto;
+        }
         .card-img-overlay {overflow:hidden}
     `]
 })
