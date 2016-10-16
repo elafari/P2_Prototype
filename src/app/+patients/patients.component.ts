4b77a47d75ada3918b22d0066c8e527122f0c971
Rafael Bamert <rafael.bamert@gmail.com>
2016-10-16 23:25:28 +0200

    work in progress on patients

--- src/app/+patients/patients.component.ts
+++ src/app/+patients/patients.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Component({
     templateUrl: './patients.component.html',
@@ -8,15 +9,59 @@ import { Component, OnInit } from '@angular/core';
             width: 100%;
             height: auto;
         }
+        .card {
+            cursor: pointer;
+        }
+        .card:hover {
+            background-color: azure;
+        }
         .card-img-overlay {overflow:hidden}
     `]
 })
-export class PatientsComponent implements OnInit {
+export class PatientsComponent {
 
-    constructor() {
+    route: string = 'patients';
+    patients: FirebaseListObservable<any>;
+    
+    constructor(private angularFire: AngularFire) {
+        this.patients = angularFire.database.list('patients', {
+            query: {
+                orderByKey: true
+            }
+        });
+        debugger;
+        console.log('this.patients', this.patients);
     }
 
-    ngOnInit() {
+    showPatient(patientKey: string) {
+        alert('New Route: ' + this.route + '/' + patientKey);
     }
 
+    getMostRecentCase(patientKey: string) {
+        debugger;
+        /*this.patients.forEach(function(val, idx, array) {
+            debugger;
+        });*/
+        this.patients.map(function(patient) {
+            debugger;
+        });
+
+        return 'tbd';
+    }
+
+    addPatient(newName: string) {
+        this.patients.push({name: newName});
+    }
+
+    updatePatient(key: string, newText: string) {
+        this.patients.update(key, {name: newText});
+    }
+
+    deletePatient(key: string) {
+        this.patients.remove(key);
+    }
+
+    deletePatientAll() {
+        //this.patients.remove();
+    }
 }
